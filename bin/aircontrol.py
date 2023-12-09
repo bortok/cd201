7cf8642259ef97c3e12a0410ba7c1a1ef2cf7188
Alex Bortok <431965+bortok@users.noreply.github.com>
2023-12-08 22:20:52 -0800

    aircontrol.py

---
+++ bin/aircontrol.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import sys
+import re
+
+artist = ""
+track = ""
+album = ""
+
+def extract(line):
+    m = re.match('^(Title|Artist|Album Name): \"(.*?)\"\.$', line)
+    if m:
+        return m.group(1), m.group(2)
+    else:
+        return None, None
+
+def update(key, val):
+    global artist, album, track
+    if key == "Artist":
+        artist = val
+    elif key == "Album Name":
+        album = val
+    elif key == "Title":
+         track = val
+
+def render():
+    print("%s - %s - %s", artist, album, track)
+    sys.stdout.flush()
+
+# Main loop
+try:
+    while True:
+        line = sys.stdin.readline()
+        key, val = extract(line)
+        if key and val:
+            update(key, val)
+            render()
+
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    pass
